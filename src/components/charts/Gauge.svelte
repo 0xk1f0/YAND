290e7b29c1e9a4e481f876c45f416538047cc42d
0xk1f0 <fteppan@protonmail.com>
2023-11-18 17:31:39 +0100

    add apexcharts and wrappers for it

---
+++ src/components/charts/Gauge.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+    import ApexCharts from 'apexcharts';
+    import { onMount } from 'svelte';
+
+    export let name: string;
+    export let value: number = 0;
+    export let color: string[] = ['#000000'];
+
+    var options = {
+        series: [value],
+        chart: {
+            type: 'radialBar',
+            offsetY: 20,
+        },
+        plotOptions: {
+            radialBar: {
+                startAngle: -90,
+                endAngle: 90,
+                dataLabels: {
+                    name: {
+                        show: false
+                    },
+                    value: {
+                        offsetY: -2,
+                        fontSize: '24px'
+                    }
+                }
+            }
+        },
+        legend: {
+            show: false,
+        },
+        tooltip: {
+            enabled: false,
+        },
+        fill: {
+            type: 'gradient',
+            colors: color,
+            gradient: {
+                opacityFrom: 1,
+                opacityTo: 0.7,
+                stops: [0, 100]
+            },
+        },
+    };
+
+    onMount(() => {
+        const chart = new ApexCharts(document.getElementById(name), options);
+        chart.render();
+
+        return {
+            destroy() {
+                chart.destroy();
+            }
+        };
+    });
+</script>
+
+<div id={name} />
---
+++ src/components/charts/LineChart.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+    import ApexCharts from 'apexcharts';
+
+    export let xData: number[] = [0];
+    export let yData: number[] = [0];
+    export let color: string[] = ['#000000'];
+    export let showLabels: boolean = true;
+
+    var options = {
+        series: [
+            {
+                data: yData
+            }
+        ],
+        chart: {
+            type: 'area',
+            zoom: {
+                enabled: false
+            },
+            toolbar: {
+                show: false
+            }
+        },
+        legend: {
+            show: false
+        },
+        tooltip: {
+            enabled: false
+        },
+        dataLabels: {
+            enabled: false
+        },
+        stroke: {
+            curve: 'smooth',
+            colors: color
+        },
+        grid: {
+            show: showLabels,
+            borderColor: color[1],
+            row: {
+                colors: color,
+                opacity: 0
+            },
+            column: {
+                colors: color,
+                opacity: 0
+            }
+        },
+        xaxis: {
+            type: 'numeric',
+            categories: xData,
+            tooltip: {
+                enabled: false
+            },
+            crosshairs: {
+                show: false
+            },
+            labels: {
+                show: showLabels,
+                style: {
+                    colors: color
+                }
+            }
+        },
+        yaxis: {
+            labels: {
+                show: showLabels,
+                style: {
+                    colors: color
+                }
+            },
+            tooltip: {
+                enabled: false
+            },
+            crosshairs: {
+                show: false
+            }
+        },
+        fill: {
+            colors: color
+        }
+    };
+
+    function chart(node: any) {
+        const chart = new ApexCharts(node, options);
+        chart.render();
+
+        return {
+            destroy() {
+                chart.destroy();
+            }
+        };
+    }
+</script>
+
+<div id="apex-chart" use:chart />
