239a66127526aaa2d4b8591345d7d850bb7c1f11
0xk1f0 <dev@k1f0.dev>
2024-05-23 00:42:13 +0200

    fix(scaling): dont move badge out of bounds

--- app/src/lib/config.ts
+++ app/src/lib/config.ts
@@ -1,68 +1,77 @@
-import path from 'path';
-import { promises as fs } from 'fs';
-import { parse } from 'yaml'
+import path from "path";
+import { promises as fs } from "fs";
+import { parse } from "yaml";
 
-const CONFIG_DIR = process.env.CONFIG_DIR || '/config/config.yaml';
-const DEFAULT_DIR = process.env.DEFAULT_DIR || '/default/default.yaml';
+const CONFIG_DIR = process.env.CONFIG_DIR || "/config/config.yaml";
+const DEFAULT_DIR = process.env.DEFAULT_DIR || "/default/default.yaml";
 
 class ConfigParser {
     async getGeneral() {
-        let config = '';
+        let config = "";
         try {
-            config = await fs.readFile(path.normalize(CONFIG_DIR), 'utf-8');
+            config = await fs.readFile(path.normalize(CONFIG_DIR), "utf-8");
         } catch {
             try {
-                config = await fs.readFile(path.normalize(DEFAULT_DIR), 'utf-8');
+                config = await fs.readFile(
+                    path.normalize(DEFAULT_DIR),
+                    "utf-8"
+                );
             } catch {
-                config = '';
+                config = "";
             }
         }
-        if (config !== '') {
+        if (config !== "") {
             const YAML = parse(config);
             return YAML;
         } else {
-            throw Error("No config file found")
-        };
+            throw Error("No config file found");
+        }
     }
 
     async getCategories() {
-        let config = '';
+        let config = "";
         try {
-            config = await fs.readFile(path.normalize(CONFIG_DIR), 'utf-8');
+            config = await fs.readFile(path.normalize(CONFIG_DIR), "utf-8");
         } catch {
             try {
-                config = await fs.readFile(path.normalize(DEFAULT_DIR), 'utf-8');
+                config = await fs.readFile(
+                    path.normalize(DEFAULT_DIR),
+                    "utf-8"
+                );
             } catch {
-                config = '';
+                config = "";
             }
         }
-        if (config !== '') {
+        if (config !== "") {
             const YAML = parse(config);
             return YAML.services;
         } else {
-            throw Error("No config file found")
-        };
+            throw Error("No config file found");
+        }
     }
 
     async getCategoryEntries(category: string) {
-        let config = '';
+        let config = "";
         try {
-            config = await fs.readFile(path.normalize(CONFIG_DIR), 'utf-8');
+            config = await fs.readFile(path.normalize(CONFIG_DIR), "utf-8");
         } catch {
             try {
-                config = await fs.readFile(path.normalize(DEFAULT_DIR), 'utf-8');
+                config = await fs.readFile(
+                    path.normalize(DEFAULT_DIR),
+                    "utf-8"
+                );
             } catch {
-                config = '';
+                config = "";
             }
         }
-        if (config !== '') {
+        if (config !== "") {
             const YAML = parse(config);
-            const SERVICES: { name: string, items: any }[] = YAML.services;
+            const SERVICES: { name: string; items: any }[] = YAML.services;
             const ITEMS = SERVICES.find((element) => element.name === category);
-            return ITEMS !== undefined ? ITEMS.items: [];
+            return ITEMS !== undefined ? ITEMS.items : [];
         } else {
-            throw Error("No config file found")
-        };
+            throw Error("No config file found");
+        }
     }
 }
 
