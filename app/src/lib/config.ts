4f0932d06316f32ba054ae3576f0f61ef6fea982
0xk1f0 <dev@k1f0.dev>
2024-05-23 20:01:50 +0200

    feat(img): preload known icons

--- app/src/lib/config.ts
+++ app/src/lib/config.ts
@@ -6,7 +6,7 @@ const CONFIG_DIR = process.env.CONFIG_DIR || "/config/config.yaml";
 const DEFAULT_DIR = process.env.DEFAULT_DIR || "/default/default.yaml";
 
 class ConfigParser {
-    async getGeneral() {
+    async readConfig() {
         let config = "";
         try {
             config = await fs.readFile(path.normalize(CONFIG_DIR), "utf-8");
@@ -28,50 +28,29 @@ class ConfigParser {
         }
     }
 
-    async getCategories() {
-        let config = "";
-        try {
-            config = await fs.readFile(path.normalize(CONFIG_DIR), "utf-8");
-        } catch {
-            try {
-                config = await fs.readFile(
-                    path.normalize(DEFAULT_DIR),
-                    "utf-8"
-                );
-            } catch {
-                config = "";
-            }
-        }
-        if (config !== "") {
-            const YAML = parse(config);
-            return YAML.services;
-        } else {
-            throw Error("No config file found");
-        }
+    async getCategoryEntries(yaml: any, category: string) {
+        const SERVICES: { name: string; items: any }[] = yaml.services;
+        const ITEMS = SERVICES.find((element) => element.name === category);
+        return ITEMS !== undefined ? ITEMS.items : [];
     }
 
-    async getCategoryEntries(category: string) {
-        let config = "";
-        try {
-            config = await fs.readFile(path.normalize(CONFIG_DIR), "utf-8");
-        } catch {
-            try {
-                config = await fs.readFile(
-                    path.normalize(DEFAULT_DIR),
-                    "utf-8"
-                );
-            } catch {
-                config = "";
-            }
-        }
-        if (config !== "") {
-            const YAML = parse(config);
-            const SERVICES: { name: string; items: any }[] = YAML.services;
-            const ITEMS = SERVICES.find((element) => element.name === category);
-            return ITEMS !== undefined ? ITEMS.items : [];
-        } else {
-            throw Error("No config file found");
-        }
+    async getAllIcons(yaml: any) {
+        const SERVICES: { name: string; items: any }[] = yaml.services;
+        let icons: Set<string> = new Set();
+        SERVICES.forEach((element) => {
+            element.items.forEach(
+                (entry: {
+                    name: string;
+                    logo: string;
+                    subtitle: string;
+                    tag: string;
+                    url: string;
+                }) => {
+                    icons.add(entry.logo);
+                }
+            );
+        });
+        return icons;
     }
 }
 
